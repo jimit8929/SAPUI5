fabbd636c53b9c73a522c920c849d73fadb53b0e
jimit8929 <jimit8929@gmail.com>
2025-12-31 17:04:58 +0530

    Modules and Translatable Texts

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,44 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
-  "use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
+  ],
+  (Controller, MessageToast, JSONModel, ResourceModel) => {
+    "use strict";
 
-  return Controller.extend("ui5.quickstart.controller.App", {
-    onShowHello: function () {
-      alert("Hello World");
-    },
-  });
-});
+    return Controller.extend("ui5.quickstart.controller.App", {
+      onInit: function () {
+        const oData = {
+          recipient: {
+            name: "World",
+          },
+          course: {
+            name: "SAPUI5",
+          },
+        };
+
+        const data = new JSONModel(oData);
+        this.getView().setModel(data, "dataModel");
+
+        //set i18n model on view
+        const i18nModel = new ResourceModel({
+          bundleName: "ui5.quickstart.i18n.i18n",
+        });
+        this.getView().setModel(i18nModel, "i18n");
+      },
+
+      onShowHello: function () {
+        // alert("Hello World");
+        const sText = this.getView()
+          .getModel("i18n")
+          .getResourceBundle()
+          .getText("helloMsg", ["0", "1", 2, "Jimit"]);
+
+        console.log(sText);
+        MessageToast.show("Hello World From Message Toast");
+      },
+    });
+  }
+);
