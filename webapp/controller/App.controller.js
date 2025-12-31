5b206d7d25b200761a01ee6d012b05a9b1a70db8
jimit8929 <jimit8929@gmail.com>
2026-01-01 03:52:41 +0530

    Pages_Panels, Custom_css, Nested Views

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,19 +1,5 @@
-sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-  (Controller, MessageToast) => {
-    "use strict";
+sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
+  "use strict";
 
-    return Controller.extend("ui5.quickstart.controller.App", {
-      onShowHello: function () {
-        // alert("Hello World");
-        const sText = this.getView()
-          .getModel("i18n")
-          .getResourceBundle()
-          .getText("helloMsg", ["0", "1", 2, "Jimit"]);
-
-        console.log(sText);
-        MessageToast.show("Hello World From Message Toast");
-      },
-    });
-  }
-);
+  return Controller.extend("ui5.quickstart.controller.App", {});
+});
